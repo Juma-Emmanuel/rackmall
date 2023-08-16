8a0c770b94a1f7687d9b6b3f7732471be0e5ab86
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-16 13:09:45 +0300

    updates

--- rackapp/urls.py
+++ rackapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_data_view, name="post_data_view"),
-    path('data-list/', views.data_list_view, name="data_list_view"),
+    #path('', views.post_data_view, name="post_data_view"),
+    #path('data-list/', views.data_list_view, name="data_list_view"),
+    path('', views.data_list_view, name="data_list_view"),
 ]
