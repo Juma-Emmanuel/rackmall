48a59795cca26f083dd3394e59e2ad140bfb6035
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-16 13:33:04 +0300

    del branches

--- rackapp/urls.py
+++ rackapp/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    #path('', views.data_list_view, name="data_list_view"),
+    path('', views.data_list_view, name="data_list_view"),
     #path('', views.index, name="index"),
-    path('', views.post_data_view, name="post_data_view"),
+    #path('', views.post_data_view, name="post_data_view"),
     path('data-list/', views.data_list_view, name="data_list_view"),
 ]
