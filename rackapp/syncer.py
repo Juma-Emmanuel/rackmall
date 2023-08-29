f1153965dcd2bf9134f8bb5cfee4804bfdbcb6d3
Juma-Emmanuel <jumaemmanuel462@yahoo.com>
2023-08-29 06:55:11 +0300

    Undugu ni Kufaana

--- rackapp/syncer.py
+++ rackapp/syncer.py
@@ -1,47 +1,72 @@
 import firebase_admin
 from firebase_admin import db
 from .models import F_Cart, F_CartProduct
-from .firebase_utils import *
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 # Function to post data to Firebase
-ref = db.reference('')
-def post_cart_to_firebase(cart):
-    # Initialize Firebase Admin SDK (replace with your Firebase configuration)
-      # Replace with your Firebase URL
-    
-    # Get data from Django admin database
-    #carts = F_Cart.objects.all()
 
-    #for cart in carts:
-        # Create a new cart node in Firebase
-    cart_ref = ref.child('carts').push({
-            'customer_id': cart.customer.id if cart.customer else None,
-            'total': cart.total
-        })
 
-    cart_id = cart_ref.key
-
-        # Get cart products for this cart
-    cart_products = F_CartProduct.objects.filter(cart=cart)
-
-    for cart_product in cart_products:
-            # Create a new cart product node in Firebase
-            ref.child('cart_products').child(cart_id).push({
-                'product_id': cart_product.product.id,
-                'rate': cart_product.rate,
-                'quantity': cart_product.quantity,
-                'subtotal': cart_product.subtotal
-            })
-# Create a receiver function to listen for new cart data being saved in Django admin database
 @receiver(post_save, sender=F_Cart)
-def cart_post_save(sender, instance, **kwargs):
-    post_cart_to_firebase(instance)
-
-# Create a receiver function to listen for new cart product data being saved in Django admin database
+def update_or_create_cart_product(sender, instance, created, **kwargs):
+    # This function will be called after saving an F_CartProduct object
+    # Use the 'created' flag to determine if it's a new creation or an update
+    if created:
+        # This is a new creation, so update Firebase accordingly
+        # You can use the instance to get the data you want to send to Firebase
+        data_to_update = {
+            'customer_id': instance.customer.id if instance.customer else None,
+            'total': instance.total
+        }
+        ref = db.reference('carts')  #
+        ref.child(str(instance.id)).set(data_to_update)
+    else:
+        # This is an update, you can handle it here if needed
+        # This is an update, so update Firebase accordingly
+        # You can use the instance to get the data you want to send to Firebase
+        data_to_update = {
+          'customer_id': instance.customer.id if instance.customer else None,
+            'total': instance.total
+        }
+        ref = db.reference('carts')  
+        ref.child(str(instance.id)).update(data_to_update)
 @receiver(post_save, sender=F_CartProduct)
-def cart_product_post_save(sender, instance, **kwargs):
-    if instance.cart and instance.cart.id:
-        post_cart_to_firebase(instance.cart)
+def update_or_create_cart_product(sender, instance, created, **kwargs):
+    # This function will be called after saving an F_CartProduct object
+    # Use the 'created' flag to determine if it's a new creation or an update
+    if created:
+        # This is a new creation, so update Firebase accordingly
+        # You can use the instance to get the data you want to send to Firebase
+        data_to_update = {
+            'product_id': instance.product.id,
+            'rate': instance.rate,
+            'quantity': instance.quantity,
+            'subtotal': instance.subtotal
+        }
+        ref = db.reference('cart_products')  # Reference to the 'cart_products' node in Firebase
+        ref.child(str(instance.id)).set(data_to_update)
+    else:
+        # This is an update, you can handle it here if needed
+        # This is an update, so update Firebase accordingly
+        # You can use the instance to get the data you want to send to Firebase
+        data_to_update = {
+            'product_id': instance.product.id,
+            'rate': instance.rate,
+            'quantity': instance.quantity,
+            'subtotal': instance.subtotal
+        }
+        ref = db.reference('cart_products')  # Reference to the 'cart_products' node in Firebase
+        ref.child(str(instance.id)).update(data_to_update)
+@receiver(post_delete, sender=F_CartProduct)
+def delete_cart_product(sender, instance, **kwargs):
+    # This function will be called whenever an F_CartProduct object is deleted
+    # You can use the instance to get the ID of the deleted object
+    cart_product_id = instance.id
+
+    # Now, you can update Firebase to reflect the deletion
+    ref = db.reference('cart_products')  # Reference to the 'cart_products' node in Firebase
+    ref.child(str(cart_product_id)).delete()
+
+
 # Call the function to post data to Firebase
 #post_data_to_firebase()
+
